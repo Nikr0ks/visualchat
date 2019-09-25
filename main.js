d9db691532a39ffcd76074897d3e1cde56f381ef
Nikr0ks <oookingofhome@gmail.com>
2019-09-25 17:20:13 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const webs = require('ws'),
-    Client = [],
+    Clients = [],
     server = webs.Server({port: 8080});
 
 
-server.on('connection', ws => {
-    Client.push(ws);
-    server.on()
+server.on('connection', function connection(ws) {
+    Clients.push(ws);
+    ws.on("message", function incoming(message) {
+        console.log('received: %s', message);
+        broadcastToAll(message);
+    });
 });
+
+function broadcastToAll(msg){
+    server.clients.forEach(function each(client){
+        if(client !== ws && client.readyState === webs.OPEN){
+            client.send(msg);
+        }
+    });
+};
